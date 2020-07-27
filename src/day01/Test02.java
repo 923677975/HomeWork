62f59d27e67aff243a0abd9eb45b08f9e66fdf58
92367 <923677975@qq.com>
2020-07-27 10:17:10 +0800

    7.27更新

--- src/day01/Test02.java
+++ src/day01/Test02.java
@@ -5,20 +5,21 @@ package day01;
  * 然后将"大家好!我是程序员!"修改为:"大家好!我是优秀的程序员!"并输出
  * 然后再修改为:"大家好!我是牛牛的程序员!"并输出
  * 然后在修改为:"我是牛牛的程序员!"并输出
+ *
  * @author Bonnie
  * 使用StringBuilder完成
  */
 public class Test02 {
-	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder();
-		System.out.println(sb.append("大家好!"));
-		sb.delete(0,sb.length());
-		System.out.println(sb.append("大家好!我是程序员!"));
-		sb.delete(0,sb.length());
-		System.out.println(sb.append("大家好!我是优秀的程序员!"));
-		sb.delete(0,sb.length());
-		System.out.println(sb.append("大家好!我是牛牛的程序员!"));
-		sb.delete(0,sb.length());
-		System.out.println(sb.append("我是牛牛的程序员!"));
-	}
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.append("大家好!"));
+        sb.append("我是程序员!");
+        System.out.println(sb);
+        sb.insert(6, "优秀");
+        System.out.println(sb);
+        sb.replace(6, 8, "牛牛");
+        System.out.println(sb);
+        sb.delete(0, 4);
+        System.out.println(sb);
+    }
 }
--- src/day01/Test04.java
+++ src/day01/Test04.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 
 /**
  * 要求用户从控制台输入一个email地址，然后获取该email的用户名(@之前的内容)
- * @author Bonnie
  *
+ * @author Bonnie
  */
 public class Test04 {
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		System.out.println("请输入一个email地址：");
-		String str =sc.next();
-		int a = str.indexOf("@");
-		String s = str.substring(0,a);
-		System.out.println(s);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个email地址：");
+        String str = sc.next();
+        if (str.indexOf("@") == -1) {
+            System.out.println("不存在！");
+        } else {
+            String s = str.substring(0, str.indexOf("@"));
+            System.out.println(s);
+        }
+    }
 }
